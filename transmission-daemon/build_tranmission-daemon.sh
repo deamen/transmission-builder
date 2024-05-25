5b893a9795a606dda028932a2d5bf8aeeac12999
Song Tang <stang@mmz.au>
2024-05-25 20:27:34 +1000

    Pin  alpinelinux version to 3.20
    In alpinelinux 3.20 libzstd is required.
    The libpsl.so is included in curl-dev which will cause cmake to use it,
    added -DPSL_PREFER_STATIC_LIB=ON cmake flag and libpsl-static apk,
    so the alpine library is used.
    
    Add workflow for building transmission-daemon

--- transmission-daemon/build_tranmission-daemon.sh
+++ transmission-daemon/build_tranmission-daemon.sh
@@ -5,9 +5,7 @@ if [ ! -d out ]
 then
     mkdir out
 fi
-
-export TRANSMISSION_VERSION="4.0.5"
-buildah bud -f Containerfile.transmission --iidfile out/transmission-${TRANSMISSION_VERSION}.iid
+buildah bud --build-arg TRANSMISSION_VERSION=$TRANSMISSION_VERSION -f Containerfile.transmission --iidfile out/transmission-${TRANSMISSION_VERSION}.iid
 
 export iid=$(cat out/transmission-${TRANSMISSION_VERSION}.iid | cut -d ':' -f2)
 export container=$(buildah from ${iid})
@@ -20,6 +18,12 @@ cp -f $mnt/$1 ./out/$2
 buildah umount $container
 EOF
 chmod a+x $copy_script
-buildah unshare ./$copy_script transmission-${TRANSMISSION_VERSION}-obj/daemon/transmission-daemon transmission-daemon-4.0.5
+buildah unshare ./$copy_script transmission-${TRANSMISSION_VERSION}-obj/daemon/transmission-daemon transmission-daemon-${TRANSMISSION_VERSION}
 rm ./$copy_script
 buildah rm $container
+
+# Delete out/transmission-${TRANSMISSION_VERSION}.iid if it exists
+if [ -f out/transmission-${TRANSMISSION_VERSION}.iid ]
+then
+    rm -f out/transmission-${TRANSMISSION_VERSION}.iid
+fi
