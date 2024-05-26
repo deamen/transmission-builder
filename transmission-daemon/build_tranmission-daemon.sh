2793ff9bbe62f10a8bdd67222f0a7d2c526475c6
Song Tang <stang@mmz.au>
2024-05-26 12:29:47 +1000

    Build arm64 and amd64 transmission-daemon

--- transmission-daemon/build_tranmission-daemon.sh
+++ transmission-daemon/build_tranmission-daemon.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Create "out" folder if it does not exists
-if [ ! -d out ]
-then
-    mkdir out
+# Check if architecture is passed as argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <architecture>"
+  exit 1
 fi
-buildah bud --build-arg TRANSMISSION_VERSION=$TRANSMISSION_VERSION -f Containerfile.transmission --iidfile out/transmission-${TRANSMISSION_VERSION}.iid
 
-export iid=$(cat out/transmission-${TRANSMISSION_VERSION}.iid | cut -d ':' -f2)
+ARCH=$1
+
+# Create "out" folder if it does not exist
+if [ ! -d out ]; then
+  mkdir out
+fi
+
+buildah bud --build-arg TRANSMISSION_VERSION=$TRANSMISSION_VERSION --platform linux/$ARCH -f Containerfile.transmission --iidfile out/transmission-${TRANSMISSION_VERSION}-${ARCH}.iid
+
+export iid=$(cat out/transmission-${TRANSMISSION_VERSION}-${ARCH}.iid | cut -d ':' -f2)
 export container=$(buildah from ${iid})
 
 copy_script="copy_artifacts.sh"
@@ -18,12 +26,11 @@ cp -f $mnt/$1 ./out/$2
 buildah umount $container
 EOF
 chmod a+x $copy_script
-buildah unshare ./$copy_script transmission-${TRANSMISSION_VERSION}-obj/daemon/transmission-daemon transmission-daemon-${TRANSMISSION_VERSION}
+buildah unshare ./$copy_script transmission-${TRANSMISSION_VERSION}-obj/daemon/transmission-daemon transmission-daemon-${TRANSMISSION_VERSION}-${ARCH}
 rm ./$copy_script
 buildah rm $container
 
-# Delete out/transmission-${TRANSMISSION_VERSION}.iid if it exists
-if [ -f out/transmission-${TRANSMISSION_VERSION}.iid ]
-then
-    rm -f out/transmission-${TRANSMISSION_VERSION}.iid
+# Delete out/transmission-${TRANSMISSION_VERSION}-${ARCH}.iid if it exists
+if [ -f out/transmission-${TRANSMISSION_VERSION}-${ARCH}.iid ]; then
+  rm -f out/transmission-${TRANSMISSION_VERSION}-${ARCH}.iid
 fi
